575aec2b925e8ea163bfd6f119f22c525842f21c
DavidBanda <davidbandag96@gmail.com>
2020-06-11 15:29:52 -0600

    Add a check_reboot function

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,12 @@
+import os
+
+def check_reboot():
+    """Returns True if the computer has a pending reboot"""
+    return os.path.exists('/run/reboot-required')
+
 def main():
     print(f'Hello Git from {__name__}')
+    
 
 if __name__ == '__main__':
     main()
