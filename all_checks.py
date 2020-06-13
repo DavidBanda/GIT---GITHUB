b91433c46e916bf077e6dcea4f59effa9d15b18c
David Aaron Banda Gutierrez <davidbandag96@gmail.com>
2020-06-13 14:57:20 -0600

    update notes.txt file

--- all_checks.py
+++ all_checks.py
@@ -1,14 +1,16 @@
 import os, sys
 
+
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
     return os.path.exists('/run/reboot-required')
+
 
 def main():
     if check_reboot():
         print('Pending Reboot.')
         sys.exit(1)
-    print('Everything ok.')
+    print(f'Everything ok')
     sys.exit(0)
     
 
