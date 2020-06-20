3044d41a843ff58872cf25f7c1e5f0cd3a35bcb9
David Banda <davidbandag96@gmail.com>
2020-06-20 16:11:44 -0600

    Add check disk usage function

--- all_checks.py
+++ all_checks.py
@@ -1,5 +1,17 @@
-import os, sys
+import os, sys, shutil
 
+def check_disk_usage(disk, min_absolute, min_percent):
+    """Return True if there is enough free disk space, false otherwise"""
+    du = shutil.disk_usage(disk)
+
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
 
 def check_reboot():
     """Returns True if the computer has a pending reboot"""
--- free_memory.py
+++ free_memory.py
@@ -1,7 +1,20 @@
-def main():
-    """Checks if there's enough free memory in the computer"""
-    print('Everything is ok')
+from psutil import virtual_memory
+from math import floor
 
-main()
+def check_free_memory():
+    memory = virtual_memory()
+    BYTES_IN_GIGABYTE = 1000000000
+
+    totalMemoryInBytes = memory.total
+    totalMemoryInGB = floor(totalMemoryInBytes / BYTES_IN_GIGABYTE)
+    freeMemoryInBytes = memory.free
+    freeMemoryInGB = freeMemoryInBytes / BYTES_IN_GIGABYTE
+
+    print(f'Total physical memory available: {totalMemoryInGB}GB')
+    print(f'Free physical memory available: {freeMemoryInGB}GB')
+
+if __name__ == '__main__':
+    check_free_memory()
+
 
 
